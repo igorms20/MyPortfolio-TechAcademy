6056c2840241d15eaf023f8db503ef42ebfe6d45
Andre Igor <andreigormenezes@gmail.com>
2022-06-01 20:19:57 -0300

    back-to-top button

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,6 @@
 function scrollEvent() {
     const aboutmeSection = document.getElementById('aboutme')
+    const backtopButton = document.getElementById('back-top')
     if (innerWidth <= 1000) {
         if (scrollY >= 3500) {
             aboutmeSection.classList.add('active')        
@@ -8,6 +9,12 @@ function scrollEvent() {
     if (innerWidth > 1000) {
         if (scrollY >= 1500) {
             aboutmeSection.classList.add('active')
+        }
+
+        if (scrollY >= 1220) {
+            backtopButton.classList.add('show')
+        } else {
+            backtopButton.classList.remove('show')
         }
     }
 }
